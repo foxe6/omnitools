9a3a6e6cb2361c6251a5988ef91434471074331a
foxe6-temp <foxe6@protonmail.com>
2020-04-15 11:04:19 +0800

    Automatic Deployment: Unknown Commitment

--- omnitools/__init__.py
+++ omnitools/__init__.py
@@ -4,7 +4,8 @@ from .encoding import *
 from .hashing import *
 from .rng import *
 from .js import *
+from .inspecting import *
 
-version = "0.0.68"
+version = "0.0.69"
 keywords = ["omnitools utilities"]
 entry = "test:main"

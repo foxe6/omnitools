6a1a6f9a845b05fb44d58e57dc7b0909c10a2d82
foxe6-temp <foxe6@protonmail.com>
2020-06-03 02:00:08 +0800

    Automatic Deployment: Unknown Commitment

--- omnitools/imaging.py
+++ omnitools/imaging.py
@@ -4,7 +4,7 @@ import PIL.ImageFont
 __ALL__ = "text_with_padding"
 
 
-def text_with_padding(font: PIL.ImageFont.FreeTypeFont, text: str, padding: int = -1) -> tuple:
+def textbox(font: PIL.ImageFont.FreeTypeFont, text: str, padding: int = -1) -> tuple:
     ascent, descent = font.getmetrics()
     (width, baseline), (offset_x, offset_y) = font.font.getsize(text)
     height = ascent+descent
@@ -12,5 +12,5 @@ def text_with_padding(font: PIL.ImageFont.FreeTypeFont, text: str, padding: int 
     if padding < 0:
         padding = offset_y+descent
     shape = (width+padding, height+padding)
-    pos = (padding/2, padding/2+y_offset)
+    pos = (padding/2, padding/2+y_offset/2)
     return (shape, pos)

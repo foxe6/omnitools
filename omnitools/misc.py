d4dba71b30d11f1029f6a62e126071ba10245932
foxe6-temp <foxe6@protonmail.com>
2020-06-19 10:42:37 +0800

    Automatic Deployment: Unknown Commitment

--- omnitools/misc.py
+++ omnitools/misc.py
@@ -1,3 +1,10 @@
-def args(*args, **kwargs):
-    return args, kwargs
+def args(*args, **kwargs) -> tuple:
+    return (args, kwargs)
+
+
+def def_template(_def, *args, **kwargs):
+    def __def():
+        return _def(*args, **kwargs)
+
+    return __def
 

374900708e06bbaa82b18ac49b0d78317bdd0640
foxe6-temp <foxe6@protonmail.com>
2020-11-27 14:12:30 +0800

    Automatic Deployment: Unknown Commitment

--- omnitools/rng.py
+++ omnitools/rng.py
@@ -15,5 +15,5 @@ def randi(power: int = 6) -> int:
     return random.randint(10 ** power, 10 ** (power + 1) - 1)
 
 
-def randstr(length: int, extra_str: str) -> str:
+def randstr(length: int, extra_str: str = "") -> str:
     return "".join(random.SystemRandom().choice(string.ascii_letters+string.digits+extra_str) for _ in range(length))
--- setup.py
+++ setup.py
@@ -18,9 +18,12 @@ open("README.md", "wb").write(readme.encode("utf-8"))
 description = re.search(r"<i>(.*?)</i>", readme)[1]
 setup(
     name="omnitools",
-    version="0.0.98",
+    version="0.0.99",
     keywords=["omnitools utilities"],
     packages=find_packages(),
+    package_data = {
+        '': ['*.ttc'],
+    },
     url="https://github.com/foxe6/omnitools",
     license="AGPL-3.0",
     author="f̣ộx̣ệ6",
